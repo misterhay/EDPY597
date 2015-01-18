40f0af4c206eed69de7c8eebd9c3b7454219e11e
David Hay <misterhay@gmail.com>
2015-01-18 16:00:07 -0700

    added setAll for turning all pins on or off

--- pythonGPIObuttons.py
+++ pythonGPIObuttons.py
@@ -4,9 +4,9 @@
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 pins = [2, 3, 4, 17, 27, 22, 10, 9, 11, 14, 15, 18, 23, 24, 25, 8, 7]
-for i in pins: #iterate through the array list declared above
-    GPIO.setup(i, GPIO.OUT) #set up the pin as an output
-    GPIO.output(i, 1) #turn the pin off (0 is on)
+for pin in pins: #iterate through the array list declared above
+    GPIO.setup(pin, GPIO.OUT) #set up the pin as an output
+    GPIO.output(pin, 1) #turn the pin off (0 is on)
 
 from Tkinter import * #for the interface
 
@@ -18,6 +18,16 @@ def pinOut(pin, state):
     if state == 'off':
         stateNumber = 1
     GPIO.output(i, stateNumber) #
+
+def setAll(state):
+    print 'Turning them all', state
+    if state == 'on':
+        stateNumber = 0
+    if state == 'off':
+        stateNumber = 1
+    for pin in pins:
+        GPIO.output(pin, stateNumber)
+        print pin, state
 
 controlWindow = Tk() #we're going to create a window full of buttons
 controlWindow.title('GPIO Output Buttons') #set the title of the window
@@ -31,4 +41,6 @@ for pin in pins:
     Button(controlWindow, text=pin, background=onColour, command=lambda pin=pin: pinOut(pin, 'on')).grid(row=1, column=columnNumber)
     Button(controlWindow, text=pin, background=offColour, command=lambda pin=pin: pinOut(pin, 'off')).grid(row=2, column=columnNumber)
     columnNumber = columnNumber + 1 #increase this variable by one to prepare for the next iteration
+Button(controlWindow, text='all', background=onColour, command=lambda: setAll('on')).grid(row=1, column=columnNumber)
+Button(controlWindow, text='all', background=offColour, command=lambda setAll('off')).grid(row=2, column=columnNumber)
 controlWindow.mainloop() #show the window

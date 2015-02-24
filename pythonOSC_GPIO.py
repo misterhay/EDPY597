343b41857890d4262af1b18b00142db20d3b0c65
David Hay <misterhay@gmail.com>
2015-02-23 21:46:57 -0700

    working as an OSC server

--- pythonOSC_GPIO.py
+++ pythonOSC_GPIO.py
@@ -13,39 +13,40 @@ import OSC # requires pyOSC
 ##GPIO.setmode(GPIO.BCM)
 receiveAddress = ('0.0.0.0', 9000) # allow it to receive from anyone on port 9000
 server = OSC.OSCServer(receiveAddress) # set up the OSC server
-server.addDefaultHandlers() # not really necessary, but good practice
+server.addDefaultHandlers() # not really necessary, but a good idea
 
 def outputHandler(address, typetag, value, source):
    # typetag should be i for integer (1 for output on, 0 for off)
    output = int(address.split('/')[2]) # get the output number from the OSC address
    pin = outputs[output-1] # translate the output number back to a pin number from the outputs array
-   print 'pin:', pin, ' output:', output, ' value:', value #print what we've learned
+   #print 'pin:', pin, ' output:', output, ' value:', value #print what we've learned
    if relayBoard == True: # will invert the output if you set relayBoard to True
       if value == [0]:
          value = [1] # because for the relayBoard "HIGH" is "off"
       else:
          value = [0]
    if value == [0]:
-      print output, 'off'
+      print 'turning off pin', output
 ##      GPIO.output(pin, GPIO.LOW) # turn off the pin
    if value == [1]:
-      print output, 'on'
+      print 'turning on pin', output
 ##      GPIO.output(pin, GPIO.HIGH) # turn on the pin
+   '''
    else:
       print 'unknown command'
+   '''
 
 output = 1 # this is the first output, we'll iterate from here
 for pin in outputs: # iterate through pins to set them up and make sure they are off
-   print 'setting up output pin', pin, 'as output number', output
 ##   GPIO.setup(pin, GPIO.OUT)
    if relayBoard == True:
       print 'making sure the relay pin', pin, 'is off'
 ##   GPIO.output(pin, GPIO.HIGH)
-   outputMessage = '/output/'+str(outpu)
+   outputMessage = '/output/'+str(output)
    server.addMsgHandler(outputMessage, outputHandler)
-   print outputMessage
+   print 'To turn on pin', pin, 'send', outputMessage, '[1]'
    output = output + 1 # iterate the counting variable
-print 'we set up', output, 'pins as outputs'
+print 'we set up', output-1, 'pins as outputs'
 
 threadingServer = threading.Thread(target = server.serve_forever)
 threadingServer.start() # start the server thread we just defined
